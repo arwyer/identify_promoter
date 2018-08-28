d5ebeece2e08fcaa3a224c1a098eba30778f1959
arwyer <awyer@vols.utk.edu>
2018-08-28 14:27:36 -0400

    trying to upgrade six

--- deps/kb_psl/install-pyseqlogo.sh
+++ deps/kb_psl/install-pyseqlogo.sh
@@ -4,6 +4,7 @@ pip install pyBigWig
 sudo easy_install --upgrade numpy
 mkdir /tmp/python-eggs
 export PYTHON_EGG_CACHE = /tmp/python-eggs
+sudo easy_install -U six
 sudo easy_install matplotlib
 pip install pandas
 git clone https://github.com/saketkc/pyseqlogo.git
